899503e1804c30a5fdbff0c1d5be9261846ff22f
unknown <arteg132579@gmail.com>
2020-03-16 02:53:10 +0300

    feat: im;lement slider

--- index.js
+++ index.js
@@ -99,5 +99,206 @@ window.onload = () => {
             elem.style.order = `${idArr[i]}`;
         });
     });
+    ////////////////////////////////////
 
+    const rightArrow = document.querySelector('.arrow-right');
+    const slide = document.querySelector('.slide');
+    const sliderWrap = document.querySelector('.slider-wrap');
+    const vertical = document.querySelector('.vertical');
+    const horizontal = document.querySelector('.horizontal');
+    const leftArrow = document.querySelector('.arrow-left');
+    let checker = null;
+    let secondSlideAnimation,firstSlideAnimation;
+    function animation() {
+        let start = Date.now();
+        let timer = setInterval(function() {
+          let timePassed = Date.now() - start;
+          if (timePassed >= 1023) {
+            clearInterval(timer);
+            return;
+          }
+          firstSlideAnimation(timePassed);
+          secondSlideAnimation(timePassed);
+        }, 2);
+    }
+    rightArrow.addEventListener('click', () => {
+        let slide0, slide1;
+        if ((sliderContainer.style.left == '') || (sliderContainer.classList.contains('active'))) {
+            if (checker == 'left') {
+                slide0 = document.querySelector('.slide0');
+                firstSlideAnimation = timePassed => {
+                    if (document.querySelector('.slide1') !== null) {
+                        document.querySelector('.slide1').style.left = 1020 - timePassed + 'px';
+                    }
+                }
+                secondSlideAnimation = timePassed => {
+                  slide0.style.left = timePassed + 'px';
+                }
+            } else {
+                if (document.querySelector('.slide0') !== null) {
+                    sliderWrap.removeChild(document.querySelector('.slide0'));
+                }
+                slide0 = document.createElement("DIV");
+                sliderContainer.classList.remove('active');
+                sliderWrap.insertBefore(slide0, sliderWrap.firstChild);
+                slide0.classList.add('slide0');
+                firstSlideAnimation = timePassed => {
+                    if (document.querySelector('.slide1') !== null) {
+                        document.querySelector('.slide1').style.left = timePassed + 'px';
+                    } else {
+                        sliderContainer.style.left = timePassed + 'px';
+                    }
+                }
+                secondSlideAnimation = timePassed => {
+                  slide0.style.left = -1020 + timePassed + 'px';
+                }
+                if (slide0.nextElementSibling.classList.contains('for-slide0-pink')) {
+                    slide0.classList.add('for-slide0-blue');
+                } else {
+                    slide0.classList.add('for-slide0-pink');
+                    slide0.appendChild(vertical);
+                    slide0.appendChild(horizontal);
+                }
+            }
+            animation();
+        } else {
+            if (document.querySelector('.slide0') == null) {
+                slide0 = document.createElement("DIV");
+                sliderWrap.insertBefore(slide0, sliderWrap.firstChild);
+                slide0.classList.add('slide0');
+                if (slide0.nextElementSibling.classList.contains('for-slide0-pink')) {
+                    slide0.classList.add('for-slide0-blue');
+                } else {
+                    slide0.classList.add('for-slide0-pink');
+                    slide0.appendChild(vertical);
+                    slide0.appendChild(horizontal);
+                }
+            } else {
+                slide0 = document.querySelector('.slide0');
+            }
+            if (checker == 'left') {
+                slide1 = document.querySelector('.slide1');
+                firstSlideAnimation = timePassed => {
+                  slide1.style.left = timePassed + 'px';
+                }
+                secondSlideAnimation = timePassed => {
+                  slide0.style.left = -1020 + timePassed + 'px';
+                }
+            } else {
+                if (document.querySelector('.slide1') !== null) {
+                    sliderWrap.removeChild(document.querySelector('.slide1'));
+                }
+                slide1 = document.createElement("DIV");
+                sliderContainer.classList.add('active');
+                sliderWrap.insertBefore(slide1, sliderWrap.firstChild);
+                slide1.classList.add('slide1');
+                if (slide1.nextElementSibling.classList.contains('for-slide0-pink')) {
+                    slide1.classList.add('for-slide0-blue');
+                } else {
+                    slide1.classList.add('for-slide0-pink');
+                    slide1.appendChild(vertical);
+                    slide1.appendChild(horizontal);
+                }
+                firstSlideAnimation = timePassed => {
+                  slide0.style.left = timePassed + 'px';
+                }
+                secondSlideAnimation = timePassed => {
+                  slide1.style.left = -1020 + timePassed + 'px';
+                }
+            }
+            animation();
+        }
+        checker = 'right';
+    });
+
+    leftArrow.addEventListener('click', () => {
+        let slide1, slide0;
+        if ((sliderContainer.style.left == '') || (sliderContainer.classList.contains('active'))) {
+            if (checker == 'right') {
+                slide1 = document.querySelector('.slide1');
+                slide0 = document.querySelector('.slide0');
+                firstSlideAnimation = timePassed => {
+                    slide0.style.left = 1020 - timePassed + 'px';
+                }
+                secondSlideAnimation = timePassed => {
+                  slide1.style.left = -timePassed + 'px';
+                }
+            } else {
+                if (document.querySelector('.slide1') !== null) {
+                    sliderWrap.removeChild(document.querySelector('.slide1'));
+                }
+                slide1 = document.createElement("DIV");
+                sliderContainer.classList.remove('active');
+                slide1.classList.add('slide1');
+                slide1.style.left = '1020px';
+                sliderWrap.insertBefore(slide1, sliderWrap.firstChild);
+                if (slide1.nextElementSibling.classList.contains('for-slide0-pink')) {
+                    slide1.classList.add('for-slide0-blue');
+                } else {
+                    slide1.classList.add('for-slide0-pink');
+                    slide1.appendChild(vertical);
+                    slide1.appendChild(horizontal);
+                }
+
+                firstSlideAnimation = timePassed => {
+                    if (document.querySelector('.slide0') !== null) {
+                        document.querySelector('.slide0').style.left = -timePassed + 'px';
+                    } else {
+                        sliderContainer.style.left = -timePassed + 'px';
+                    }
+                }
+                secondSlideAnimation = timePassed => {
+                  slide1.style.left = 1020 - timePassed + 'px';
+                }
+            }
+            animation();
+        } else {
+            if (checker == 'right') {
+                slide0 = document.querySelector('.slide0');
+                if (document.querySelector('.slide1') !== null) {
+                    sliderWrap.removeChild(document.querySelector('.slide1'));
+                }
+                slide1 = document.createElement("DIV");
+                sliderContainer.classList.remove('active');
+                sliderWrap.insertBefore(slide1, sliderWrap.firstChild);
+                slide1.classList.add('slide1');
+                slide1.style.left = '1020px';
+                slide1.classList.add('for-slide0-pink');
+                slide1.appendChild(vertical);
+                slide1.appendChild(horizontal);
+                firstSlideAnimation = timePassed => {
+                  slide0.style.left = -timePassed + 'px';
+                }
+                secondSlideAnimation = timePassed => {
+                  slide1.style.left = 1020 - timePassed + 'px';
+                }
+            } else {
+                let n = 0;
+                if (document.querySelector('.slide0') !== null) {
+                    sliderWrap.removeChild(document.querySelector('.slide0'));
+                }
+                slide1 = document.querySelector('.slide1');
+                slide0 = document.createElement("DIV");
+                sliderContainer.classList.add('active');
+                sliderWrap.insertBefore(slide0, sliderWrap.firstChild);
+                slide0.classList.add('slide0');
+                slide0.style.left = '1020px';
+                if (slide1.classList.contains('for-slide0-pink')) {
+                    slide0.classList.add('for-slide0-blue');
+                } else {
+                    slide0.classList.add('for-slide0-pink');
+                    slide0.appendChild(vertical);
+                    slide0.appendChild(horizontal);
+                }
+                firstSlideAnimation = timePassed => {
+                  slide1.style.left = -timePassed + 'px';
+                }
+                secondSlideAnimation = timePassed => {
+                  slide0.style.left = 1020 - timePassed + 'px';
+                }
+            }
+            animation();
+        }
+        checker = 'left';
+    });
 }
